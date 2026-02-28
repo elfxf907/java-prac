ded4f1a862e7cef75a5a4c45dd88b4804834cd0a
Svetlana <kislyakova.vika@yandex.ru>
2026-02-28 03:00:08 +0300

    first commit

---
+++ src/main/java/java_prac/JavaPracApplication.java
@@ -0,0 +1,13 @@
+package java_prac;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaPracApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaPracApplication.class, args);
+	}
+
+}
---
+++ src/test/java/java_prac/JavaPracApplicationTests.java
@@ -0,0 +1,13 @@
+package java_prac;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaPracApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
